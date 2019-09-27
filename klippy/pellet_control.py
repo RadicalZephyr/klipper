b2b9a0e3d6e85c0bea4e180781d36bad12760aa3
Geoff Shannon <geoffpshannon@gmail.com>
2019-09-27 14:25:07 -0700

    Add logging to wake_handler

--- klippy/pellet_control.py
+++ klippy/pellet_control.py
@@ -83,7 +83,9 @@ class PelletControl:
             wake_time = print_time + self.off_delay_time
 
             def wake_handler(event_time):
+                logging.warn("wake_handler called at time: %.4f", event_time)
                 print_time = self.mcu.clock_to_print_time(event_time)
+                logging.warn("wake_handler calculated turn off time as: %.4f", print_time)
                 self._stop_feeding(print_time + 0.5)
                 return self.reactor.NEVER
 
