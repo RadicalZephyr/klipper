6b3f99410519685496f48ebef6d0b289026b7322
Geoff Shannon <geoffpshannon@gmail.com>
2019-10-01 17:55:29 -0700

    Add logging to observe sensor callback

--- klippy/pellet_control.py
+++ klippy/pellet_control.py
@@ -39,6 +39,7 @@ class PelletControl:
         self.mcu = blower.get_mcu()
 
     def sensor_callback(self, event_time, state):
+        logging.warn("sensor_callback called at %.2f with state: '%s'", state)
         with self.lock:
             if self.feeding:
                 logging.warn("sensor_callback(%.4f, %s)", event_time, state)
