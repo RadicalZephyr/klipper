4fac08232e22d9ca6908c38805e97bc220202d2c
Geoff Shannon <geoffpshannon@gmail.com>
2020-04-12 23:59:50 -0700

    Explicitly name the expected Due USB ports in flash.sh

--- flash.sh
+++ flash.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-PORT=/dev/ttyACM0
+PORT=/dev/serial/by-id/usb-Arduino__www.arduino.cc__Arduino_Due_Prog._Port_5573932363735191E0D2-if00
 
 # Activate the bootloader
 stty -F ${PORT} 1200 hup
 stty -F ${PORT} 9600
 sleep 3
 
-make flash FLASH_DEVICE=/dev/ttyACM0
+make flash FLASH_DEVICE=/dev/serial/by-id/usb-03eb_6124-if00
