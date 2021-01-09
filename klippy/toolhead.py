7983426f5c14c6fc1aa4cc3fdd4bf13de9c321f9
Dmitry Butyugin <dmbutyugin@google.com>
2021-01-09 15:39:43 +0100

    adxl345: Simulated accelerometer chip
    Signed-off-by: Dmitry Butyugin <dmbutyugin@google.com>

--- klippy/toolhead.py
+++ klippy/toolhead.py
@@ -249,6 +249,7 @@ class ToolHead:
         self.trapq_append = ffi_lib.trapq_append
         self.trapq_free_moves = ffi_lib.trapq_free_moves
         self.step_generators = []
+        self.move_monitoring_callbacks = []
         # Create kinematics class
         self.extruder = kinematics.extruder.DummyExtruder(self.printer)
         kin_name = config.get('kinematics')
@@ -329,6 +330,8 @@ class ToolHead:
                               + move.cruise_t + move.decel_t)
             for cb in move.timing_callbacks:
                 cb(next_move_time)
+            for cb in self.move_monitoring_callbacks:
+                cb(next_move_time, move)
         # Generate steps for moves
         if self.special_queuing_state:
             self._update_drip_move_time(next_move_time)
@@ -534,6 +537,10 @@ class ToolHead:
         last_move.timing_callbacks.append(callback)
     def note_kinematic_activity(self, kin_time):
         self.last_kin_move_time = max(self.last_kin_move_time, kin_time)
+    def register_move_monitoring_callback(self, callback):
+        self.move_monitoring_callbacks.append(callback)
+    def unregister_move_monitoring_callback(self, callback):
+        self.move_monitoring_callbacks.remove(callback)
     def get_max_velocity(self):
         return self.max_velocity, self.max_accel
     def get_max_axis_halt(self):
